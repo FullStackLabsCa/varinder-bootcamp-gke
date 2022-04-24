3643a4876bbd342523e74bdf7c0e5d27e252162c
Varinder jassal <varinderjassal@Varinders-MacBook-Pro.local>
2022-04-24 16:27:09 -0700

    changed master to main

--- src/main/java/kube/practice/DemoAppController.java
+++ src/main/java/kube/practice/DemoAppController.java
@@ -1,6 +1,5 @@
 package kube.practice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @RestController
-public class RestApi {
+public class DemoAppController {
 
 //    @Value("${spring.application.name}")
     String appName = "demoApp";
